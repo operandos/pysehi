f89bc92f65507cf3b84f21d075af2365da747af5
operandos <83643595+operandos@users.noreply.github.com>
2023-01-12 17:19:45 +0000

    Improve comparison of parameters

--- metadata.py
+++ metadata.py
@@ -8,10 +8,14 @@ Created on Thu Jan 12 14:12:09 2023
 from deepdiff import DeepDiff
 from engineering_notation import EngNumber
 
-def metadata_params(stack_meta=None, parameter=None, check_prop=False, readable=True):
+def metadata_params(stack_meta=None, parameter=None, readable=True):
     params=[]
-    if parameter is not None:
+    if type(parameter) is str:
         params.append(parameter)
+    if type(parameter) is list:
+        params=parameter
+    if stack_meta is None:
+        keys={}
     prop_list = ['curr','accel','uc','wd','r','x','y','z','hfw','average','interlacing','dwell', 'step', 'range']
     if parameter is None:
         params=prop_list
@@ -38,9 +42,12 @@ def metadata_params(stack_meta=None, parameter=None, check_prop=False, readable=
             if any(prop in s for s in ['r','x','y','z']):
                 k,unit = ['Stage',rf'Stage{prop.capitalize()}'], 'm'
             if stack_meta is None:
-                return k
+                keys[prop]=k
             if type(stack_meta) is dict:
-                value = stack_meta['img1']
+                if 'img1' in stack_meta:
+                    value = stack_meta['img1']
+                if 'img1' not in stack_meta:
+                    value = stack_meta
                 for key in k:
                     value = value.get(key)
                     if not value:
@@ -51,12 +58,36 @@ def metadata_params(stack_meta=None, parameter=None, check_prop=False, readable=
                     if len(unit)==0:
                         print('{:<15s} {:<15s}'.format(prop,value))
                 if readable is False:
-                    return k, value
+                    if parameter is not None:
+                        return k, value
+    if stack_meta is None:
+        return keys
 
-def compare_params(stack_meta_1, stack_meta_2, drop:list=None):
-    diff = DeepDiff(stack_meta_1['img1'],stack_meta_2['img1'], exclude_paths=["root['PrivateFEI']","root['PrivateFei']","root['User']", "root['System']", "root['Processing']", "root['GIS']"])
-    return diff
+def compare_params(stack_meta_1, stack_meta_2, readable=True, condition_true:list=None):
+    diff_out = DeepDiff(stack_meta_1['img1'],stack_meta_2['img1'], exclude_paths=["root['PrivateFEI']","root['PrivateFei']","root['User']", "root['System']", "root['Processing']", "root['GIS']"])
+    diff={}
+    for page in diff_out['values_changed']:
+        diff[page.split("'")[1]]={}
+    for page in diff_out['values_changed']:
+        diff[page.split("'")[1]][page.split("'")[3]]=diff_out['values_changed'][page]
+        keys = metadata_params(parameter=condition_true)
+        val = diff
+        for keyc in keys:
+            for kc in keys[keyc]:
+                val=val.get(kc)
+                if not val:
+                    break
+                print(val)
+        return diff
+    if readable is False:
+        return diff
     
-def params_warning(stack_meta):
+def params_warning(stack_meta, readable=True):
+    if round(metadata_params(stack_meta, 'wd', readable=False)[1],4) == 0.0040:
+        if readable is False:
+            return True
     if round(metadata_params(stack_meta, 'wd', readable=False)[1],4) != 0.0040:
-        print('\t\tWARNING !','\t\t', 'Working distance is \t', metadata_params(stack_meta, 'wd', readable=False)[1]*1000,' mm')
+        if readable is True:
+            print('\t\tWARNING !','\t\t', 'Working distance is \t', metadata_params(stack_meta, 'wd', readable=False)[1]*1000,' mm')
+        if readable is False:
+            return False
